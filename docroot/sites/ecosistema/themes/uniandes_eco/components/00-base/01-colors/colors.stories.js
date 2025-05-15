b32be886f7a9fc1dab15fe3dfc3ecda0705c6045
Juan Camilo Villacob <juan.villacob@esinergia.co>
2025-05-15 10:40:43 -0500

    feat: Variables, Estilos y Doc de Colors

--- docroot/sites/ecosistema/themes/uniandes_eco/components/00-base/01-colors/colors.stories.js
+++ docroot/sites/ecosistema/themes/uniandes_eco/components/00-base/01-colors/colors.stories.js
@@ -1,11 +1,25 @@
 /* eslint-disable */
 import colors from './colors.twig';
+import colorsData from './colorsPract.yml';
 
-
-import colorsData from './colors.yml';
+/**
+ * Colores de la paleta de colores del proyecto.
+ * Se pueden usar en cualquier parte del proyecto.
+ * Se recomienda usar los colores de la paleta para mantener la coherencia visual en todo el proyecto.
+ *
+ * | Class  | Color |
+ * |-------------|----------|
+ * |  bg-primary-black    |   1E1E1E   |
+ * |  bg-secondary-yellow  |  FFE41E  |
+ * |  bg-tertiary-gray  |   BAB9B3 |
+ * |  bg-background-white  |   FCFCFA |
+ * |  bg-auxiliary-gray  |   73726D |
+ * 
+ *
+ */
 
 export default {
   title: 'Base/Colors',
 };
 
-export const Main = () => colors(colorsData);
+export const Colors = () => colors(colorsData);
